1e42e53921bd349b9c7bf10b28274bc501286fda
Ece Ozalp <ozale272@newschool.edu>
2018-06-25 22:50:59 -0400

    requests can now hit the vision api

--- src/main/java/com/grammarvision/demo/domain/service/LoadingService.java
+++ src/main/java/com/grammarvision/demo/domain/service/LoadingService.java
@@ -1,12 +1,44 @@
 package com.grammarvision.demo.domain.service;
 
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.AnnotateFileResponse;
+import com.google.cloud.vision.v1.AnnotateFileResponse.Builder;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.AsyncAnnotateFileRequest;
+import com.google.cloud.vision.v1.AsyncAnnotateFileResponse;
+import com.google.cloud.vision.v1.AsyncBatchAnnotateFilesResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Block;
+import com.google.cloud.vision.v1.ColorInfo;
+import com.google.cloud.vision.v1.CropHint;
+import com.google.cloud.vision.v1.CropHintsAnnotation;
+import com.google.cloud.vision.v1.DominantColorsAnnotation;
 import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.FaceAnnotation;
 import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.GcsDestination;
+import com.google.cloud.vision.v1.GcsSource;
 import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.ImageContext;
+import com.google.cloud.vision.v1.ImageSource;
+import com.google.cloud.vision.v1.InputConfig;
+import com.google.cloud.vision.v1.LocationInfo;
+import com.google.cloud.vision.v1.OperationMetadata;
+import com.google.cloud.vision.v1.OutputConfig;
+import com.google.cloud.vision.v1.Page;
+import com.google.cloud.vision.v1.Paragraph;
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
+import com.google.cloud.vision.v1.Symbol;
+import com.google.cloud.vision.v1.TextAnnotation;
+import com.google.cloud.vision.v1.WebDetection;
+import com.google.cloud.vision.v1.WebDetection.WebEntity;
+import com.google.cloud.vision.v1.WebDetection.WebImage;
+import com.google.cloud.vision.v1.WebDetection.WebLabel;
+import com.google.cloud.vision.v1.WebDetection.WebPage;
+import com.google.cloud.vision.v1.WebDetectionParams;
+import com.google.cloud.vision.v1.Word;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors;
 import com.grammarvision.demo.domain.Picture;
@@ -29,9 +61,12 @@ import static java.util.stream.Collectors.toList;
 public class LoadingService {
 
 
-  public List<HashMap<String, String>> getAllAnnotations(List<Picture> pictures) {
+  public List<HashMap<String, String>> getAllAnnotations(List<Picture> pictures) throws IOException {
+
+    final ImageAnnotatorClient vision = ImageAnnotatorClient.create();
+
     List<byte[]> bytes = pictures.stream().map(picture -> getBytesFromUrl(picture.getPictureUrl())).collect(toList());
-    List<BatchAnnotateImagesResponse> responses = bytes.stream().map(this::getImageResponseFromBytes).collect(toList());
+    List<BatchAnnotateImagesResponse> responses = bytes.stream().map(imgBytes -> getImageResponseFromBytes(vision, imgBytes)).collect(toList());
     return singletonList(getAnnotations(responses.get(0).getResponses(0)));
   }
 
@@ -55,31 +90,29 @@ public class LoadingService {
     }
   }
 
-  public BatchAnnotateImagesResponse getImageResponseFromBytes(byte[] bytes) {
-
-    ImageAnnotatorClient vision = null;
-
-    try {
-      vision = ImageAnnotatorClient.create();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public BatchAnnotateImagesResponse getImageResponseFromBytes(ImageAnnotatorClient vision, byte[] bytes) {
 
     ByteString imgBytes = ByteString.copyFrom(bytes);
-    List<AnnotateImageRequest> request = getImageRequestFromString(imgBytes);
-    return vision.batchAnnotateImages(request);
+    AnnotateImageRequest request = getImageRequestFromString(imgBytes);
+    BatchAnnotateImagesResponse response = null;
+    try {
+      response = vision.batchAnnotateImages(singletonList(request));
+    } catch (Exception e) {
+      System.out.println("####################");
+      System.out.println(e.getMessage());
+      System.out.println("####################");
+    }
+    return response;
   }
 
-  private List<AnnotateImageRequest> getImageRequestFromString(ByteString byteString) {
-    List<AnnotateImageRequest> requests = new ArrayList<>();
+  private AnnotateImageRequest getImageRequestFromString(ByteString byteString) {
     Image img = Image.newBuilder().setContent(byteString).build();
     Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
     AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
       .addFeatures(feat)
       .setImage(img)
       .build();
-    requests.add(request);
-    return requests;
+    return request;
   }
 
   public byte[] getBytesFromUrl(String urlString) {
